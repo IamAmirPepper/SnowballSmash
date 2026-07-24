5be72abf0825ab50bc162d486b9433f657e018c5
Dean Aviv <deanaviv5@gmail.com>
2026-07-24 11:29:12 +0300

    add prefab and class for the end game canvas

---
+++ Assets/Scripts/UI/EndGameCanvas.cs
@@ -0,0 +1,84 @@
+using SnowballSmash.Gameplay;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace SnowballSmash
+{
+    public class EndGameCanvas : MonoBehaviour
+    {
+        [SerializeField] private ScoreManager scoreManager;
+        [SerializeField] private TMP_Text highscoreText;
+        [SerializeField] private Button replayButton;
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private GraphicRaycaster graphicRaycaster;
+
+        private void Awake()
+        {
+            if (canvas == null)
+            {
+                canvas = GetComponent<Canvas>();
+            }
+
+            if (graphicRaycaster == null)
+            {
+                graphicRaycaster = GetComponent<GraphicRaycaster>();
+            }
+
+            SetVisible(false);
+        }
+
+        private void OnEnable()
+        {
+            if (replayButton != null)
+            {
+                replayButton.onClick.AddListener(Replay);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (replayButton != null)
+            {
+                replayButton.onClick.RemoveListener(Replay);
+            }
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+
+            if (scoreManager != null)
+            {
+                scoreManager.SaveHighScoreIfNeeded();
+                if (highscoreText != null)
+                {
+                    highscoreText.text = scoreManager.HighScoreMeters + "m";
+                }
+            }
+
+            Time.timeScale = 0f;
+            SetVisible(true);
+        }
+
+        private void Replay()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        private void SetVisible(bool visible)
+        {
+            if (canvas != null)
+            {
+                canvas.enabled = visible;
+            }
+
+            if (graphicRaycaster != null)
+            {
+                graphicRaycaster.enabled = visible;
+            }
+        }
+    }
+}
